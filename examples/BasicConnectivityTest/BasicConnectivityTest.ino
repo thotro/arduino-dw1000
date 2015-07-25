0ad34a42bf5084aaeb4550d0be5c196fe92ad8e4
Thomas <thomas@trojer.net>
2015-07-26 01:19:39 +0200

    Apache license for all examples

--- examples/BasicConnectivityTest/BasicConnectivityTest.ino
+++ examples/BasicConnectivityTest/BasicConnectivityTest.ino
@@ -2,11 +2,19 @@
  * Copyright (c) 2015 by Thomas Trojer <thomas@trojer.net>
  * Decawave DW1000 library for arduino.
  *
- * This file is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @file BasicConnectivityTest.ino
  * Use this to test connectivity with your DW1000 from Arduino.
  * It performs an arbitrary setup of the chip and prints some information.
  */
--- examples/RangingTag/RangingTag.ino
+++ examples/RangingTag/RangingTag.ino
@@ -2,23 +2,26 @@
  * Copyright (c) 2015 by Thomas Trojer <thomas@trojer.net>
  * Decawave DW1000 library for arduino.
  *
- * This file is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Use this to test two-way ranging functionality with two
- * DW1000. This is the tag component's code which polls for range computation. 
- * Addressing and frame filtering is currently done in a custom way, as no MAC 
- * features are implemented yet.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- * Complements the "DW1000-arduino-ranging-anchor" sketch. 
+ * @file RangingTag.ino
+ * Use this to test two-way ranging functionality with two DW1000. This is
+ * the tag component's code which polls for range computation. Addressing and
+ * frame filtering is currently done in a custom way, as no MAC features are
+ * implemented yet.
+ *
+ * Complements the "RangingAnchor" example sketch. 
  */
-
- /*
-  * TODO: either ACK frames to check if message loss or watchdog timer to recover
-  * from protocol failure on tag side.
-  */
 
 #include <SPI.h>
 #include <DW1000.h>
